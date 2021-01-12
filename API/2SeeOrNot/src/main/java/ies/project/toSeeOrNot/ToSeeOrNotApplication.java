1872d57b678f105d3440f7ad50084162f4c6ec0a
Iglesias-Leafwind <pedrohpi@ua.pt>
2021-01-12 16:52:54 +0000

    Swagger implementation

--- API/2SeeOrNot/src/main/java/ies/project/toSeeOrNot/ToSeeOrNotApplication.java
+++ API/2SeeOrNot/src/main/java/ies/project/toSeeOrNot/ToSeeOrNotApplication.java
@@ -8,11 +8,13 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ JWTUtils.class })
 @EnableRabbit
 @EnableCaching
+@EnableSwagger2
 public class ToSeeOrNotApplication {
 
     public static void main(String[] args) {
